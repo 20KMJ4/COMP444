6dd2bd6b96261249a3be1744f5e656aea887993a
20KMJ4 <93559252+20KMJ4@users.noreply.github.com>
2026-04-14 16:40:17 -0400

    1A CC Morse Code

---
+++ COMP444_1A_Blink_Morse_Code.ino
@@ -0,0 +1,210 @@
+//Morse Code
+
+void setup() {
+
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  //H
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //E
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //L
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //L
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //O
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(3500);
+
+  //W
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+  
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //O
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(3500);
+
+  //R
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+  
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //L
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  //D
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(500);
+
+  digitalWrite(13, HIGH);
+  delay(500);
+
+  digitalWrite(13, LOW);
+  delay(3500);
+}
