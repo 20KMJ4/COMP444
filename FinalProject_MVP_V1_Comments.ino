147a78d0682f924ad799e2e24704f9de6815ed24
20KMJ4 <93559252+20KMJ4@users.noreply.github.com>
2026-05-29 21:12:30 -0400

    Comments for V1 of final project MVP

---
+++ FinalProject_MVP_V1_Comments.ino
@@ -0,0 +1,46 @@
+//LCD Library
+
+//Initalize Pins
+//Buttons
+//LCD
+//Ultrasonic
+//Motor
+
+// Intialize variables
+//stock
+//Price
+
+void setup() {
+  //Inputs
+  //Buttons
+  //Ultrasonic (Input/Output)
+
+  //Outputs
+  //LCD
+  //Motor
+}
+
+void loop() {
+  // Flip between two message when idle after X seconds
+  // Display a welcome message (top) and say loonies only (bottom)
+  // Display: select an item, enter coins, then press green button to confirm and dispense item (scroll)
+
+  //if green button is selected but no other button has been pressed
+    // Display must select an item 
+
+  //if blue button is pressed and have item
+    //Display selected: item name and the cost of the item
+
+  //if blue button pressed and item is out of stock
+    //Item is out of stock (top), select another item (bottom) 
+
+  //if a coin has been insterted (ultrasonic reading)
+    //Display current amount
+
+  //if enough money is entered and green button is pressed
+    //internally track inventory
+    //spin motor to dispense item
+    //Display message saying item dispensing
+    //Display amount of money returned (if applicable)
+    //Display thank you have a nice day
+}
