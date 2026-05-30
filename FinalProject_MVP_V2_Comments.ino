32cd0bc3d296d949c77b8f0d04448e232d6423dd
20KMJ4 <93559252+20KMJ4@users.noreply.github.com>
2026-05-29 21:13:41 -0400

    Re-evaluation and updated comments for MVP

---
+++ FinalProject_MVP_V2_Comments.ino
@@ -0,0 +1,47 @@
+//LCD Library
+
+//Initalize Pins
+//Buttons
+//LCD
+//Ultrasonic
+//Motor
+
+// Intialize variables
+//stock
+//Price
+
+void setup() {
+  //Inputs
+  //Buttons
+  //Ultrasonic (Input/Output)
+
+  //Outputs
+  //LCD
+  //Motor
+}
+
+void loop() {
+  // Display start message
+
+  //possible options: 1) insert coins, 2) blue button pressed, and 3) green button pressed
+    
+  //if a coin has been insterted (ultrasonic reading): add $1 to total and display current amount
+  
+  //blue button pressed has 2 options: 1) item in stock or 2) not in stock
+  //if 1) show item name and cost, become selected item
+  // if 2) says out and select another item
+
+  //green button pressed has 3 options: 1) paid but no item selected, 2) item selected but not paid and 3) item selected and paid
+  //if 1) select an item
+  // if 2) need to pay
+  // if 3) dispense item, -1 from stock, display end message
+
+  //That is a lot of possible orders
+
+  //To simplify
+  //Use Switch case: 
+  //1 Display start message
+  //2 Select item (needs to be in stock)
+  //3 Insert coins
+  //4 confirm
+}
